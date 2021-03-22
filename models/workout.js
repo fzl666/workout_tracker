5c24030b1e106669fc15dde81d59dcac7a6198d3
Luwenxi Song <songl652@newschool.edu>
2021-03-22 09:49:56 -0400

    day fix

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var workoutSchema = new Schema({
-    date: {
+    day: {
         type: Date,
         default: Date.now
       },
